a6f20d30d2b12b24a682dc35e37e15a106f1f365
Glitch (hello-webpage) <none>
2020-07-24 05:44:33 +0000

    🍽🏤 Checkpoint ./script.js:18078033/238 ./.glitch-assets:18078033/796

--- script.js
+++ script.js
@@ -5,21 +5,23 @@
  *    UP_ARROW, DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW
  */
 
-let foregroundImg,backgroundImg;
+let foregroundImg,backgroundImg,groundImg,title;
 
 function preload(){
   foregroundImg = loadImage("https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2Fsky_2.png?v=1595568118809");
   backgroundImg = loadImage("https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2Fsky.png?v=1595568225904"); 
+  groundImg = loadImage("https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2Ftop_ground.png?v=1595568970498")
 }
 
 function setup(){
   createCanvas(500,600)
   colorMode(HSB, 360, 100, 100)
-  background(190,55,100)
+  background(200,66,100)
   
   var manager = new SceneManager();
   manager.fImage = foregroundImg;
   manager.bImage = backgroundImg;
+  manager.gImage = groundImg;
   manager.wire();
   manager.showScene(Intro);
 }
@@ -28,8 +30,9 @@ function draw(){
 }
 function Intro(){
   this.setup = function(){
-    image(foregroundImg,-450,200)
-    image(backgroundImg,-450,200)
+    image(foregroundImg,-450,225)
+    image(backgroundImg,-450,255)
+    image(groundImg,-450,540)
   }
   this.draw = function(){
     
