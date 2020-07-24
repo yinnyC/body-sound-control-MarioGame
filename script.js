0a60bf05a16a20c3e3336a84804faab2aa83ac80
Glitch (hello-webpage) <none>
2020-07-24 04:56:28 +0000

    📕🎖 Checkpoint ./script.js:18078033/596

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
+// Name any p5.js functions we use in `global` so Glitch can recognize them.
+/* global
+ *    ESCAPE,textSize,image,VIDEO,createCapture,ml5,HSB, background, color, collideRectRect, colorMode, createCanvas, fill, frameRate, keyCode, height,
+ *    loop, noFill, noLoop, noStroke, random, rect, round, stroke, sqrt, text, width
+ *    UP_ARROW, DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW
+ */
+
 function setup(){
-  
+  createCanvas(500,600)
+  colorMode(HSB, 360, 100, 100)
+  background(190,55,100)
 }
 function draw(){
   
 }
 function Intro(){
   this.setup = function(){
+    
+  }
+  this.draw = function(){
     
   }
 }
