77387fae4f78de86cca3398ea1ba03d131b73551
Glitch (hello-webpage) <none>
2020-07-24 23:51:03 +0000

    🍍🤑 Checkpoint ./index.html:18078033/75 ./p5.clickable.js:18078033/4797 ./script.js:18078033/241

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 // Name any p5.js functions we use in `global` so Glitch can recognize them.
 /* global
- *    drawIntroScreen,SceneManager,loadImage,ESCAPE,textSize,image,VIDEO,createCapture,ml5,HSB, background, color, collideRectRect, colorMode, createCanvas, fill, frameRate, keyCode, height,
+ *    Clickable,drawIntroScreen,SceneManager,loadImage,ESCAPE,textSize,image,VIDEO,createCapture,ml5,HSB, background, color, collideRectRect, colorMode, createCanvas, fill, frameRate, keyCode, height,
  *    loop, noFill, noLoop, noStroke, random, rect, round, stroke, sqrt, text, width
  *    UP_ARROW, DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW
  */
 
 let foregroundImg,backgroundImg,groundImg,title;
+let soundMode,bodyMode;
 
 function preload(){
+  // load background Images
   foregroundImg = loadImage("https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2Fsky_2.png?v=1595568118809");
   backgroundImg = loadImage("https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2Fsky.png?v=1595568225904"); 
   groundImg = loadImage("https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2Ftop_ground.png?v=1595568970498");
@@ -18,7 +20,7 @@ function setup(){
   createCanvas(500,600)
   colorMode(HSB, 360, 100, 100)
   background(200,66,100)
-  
+  // set up the screen manager
   var manager = new SceneManager();
   manager.fImage = foregroundImg;
   manager.bImage = backgroundImg;
@@ -26,6 +28,9 @@ function setup(){
   manager.title = title;
   manager.wire();
   manager.showScene(Intro);
+  // set up clickable 
+  soundMode = new Clickable();
+  bodyMode = new Clickable();
 }
 
 function Intro(){
@@ -34,6 +39,7 @@ function Intro(){
     background(200,66,100)
     this.showBackground()
     this.moveBackground()
+    
   }
   this.showBackground= function(){
     image(foregroundImg,bgX,225)
