74b50da1c1811ab6c2b2c39f2fedbfe4d1fba858
Glitch (hello-webpage) <none>
2020-07-26 04:54:54 +0000

    💿🚈 Checkpoint ./script.js:18078033/10272 ./.glitch-assets:18078033/480

--- script.js
+++ script.js
@@ -7,18 +7,26 @@
 
 let foregroundImg, backgroundImg, groundImg, title;
 let soundMode, bodyMode;
-let startGround,ground,endGroud;
+let startGround, ground, endGroud;
 
 function preload() {
   // load background Images
-  foregroundImg = loadImage("https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2Fsky_2.png?v=1595568118809");
-  backgroundImg = loadImage("https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2Fsky.png?v=1595568225904");
-  groundImg = loadImage("https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2Ftop_ground.png?v=1595568970498");
-  title = loadImage("https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2FSuper.png?v=1595626658046");
-  startGround = loadImage("https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2Fstarting.png?v=1595721097175")
-
+  foregroundImg = loadImage(
+    "https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2Fsky_2.png?v=1595568118809"
+  );
+  backgroundImg = loadImage(
+    "https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2Fsky.png?v=1595568225904"
+  );
+  groundImg = loadImage(
+    "https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2Ftop_ground.png?v=1595568970498"
+  );
+  title = loadImage(
+    "https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2FSuper.png?v=1595626658046"
+  );
+  startGround = loadImage(
+    "https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2Fstarting.png?v=1595721097175"
+  );
 }
-
 
 function setup() {
   createCanvas(500, 600);
@@ -34,8 +42,8 @@ function setup() {
 }
 
 function Intro() {
-  let bgX = 0;   // To scroll the background
-  let choice  = "1"
+  let bgX = 0; // To scroll the background
+  let choice = "1";
   this.setup = function() {
     // set up clickable - Button soundMode
     soundMode = new Clickable();
@@ -43,24 +51,24 @@ function Intro() {
     soundMode.text = "Sound Mode";
     soundMode.onHover = function() {
       this.color = "#808080";
-      choice="sound"
+      choice = "sound";
     };
     soundMode.onOutside = function() {
       this.color = "#FFFFFF";
-      choice=""
+      choice = "";
     };
     // set up clickable - Button bodyMode
     bodyMode = new Clickable();
     bodyMode.locate(width / 2 - 50, 420);
     bodyMode.text = "Body Mode";
-   
+
     bodyMode.onHover = function() {
       this.color = "#808080";
-      choice = "body"
+      choice = "body";
     };
     bodyMode.onOutside = function() {
       this.color = "#FFFFFF";
-      choice=""
+      choice = "";
     };
   };
   this.draw = function() {
@@ -70,21 +78,21 @@ function Intro() {
     soundMode.draw();
     bodyMode.draw();
   };
-  this.mousePressed = function(){
-    if(choice!=""){ 
-      this.sceneManager.showScene(Game,choice)
+  this.mousePressed = function() {
+    if (choice != "") {
+      // Switch to Game scene when user click any of the buttons
+      this.sceneManager.showScene(Game, choice);
     }
-    
-  }
+  };
   this.showBackground = function() {
     image(this.sceneManager.fImage, bgX, 227);
     image(this.sceneManager.bImage, bgX, 255);
     image(this.sceneManager.gImage, bgX, 540);
     image(this.sceneManager.title, 70, 100, 350, 200);
-    if ( Math.floor(frameCount / 30) % 2 == 0 ) 
-        {
-            text("Select mode to start game", width / 2-70, height -20);
-        }
+    if (Math.floor(frameCount / 30) % 2 == 0) {
+      // A blinking Info
+      text("Select mode to start game", width / 2 - 70, height - 20);
+    }
   };
   this.moveBackground = function() {
     bgX -= 1;
@@ -95,82 +103,83 @@ function Intro() {
 }
 
 function Game() {
-  let bgX = 0,moveSpeed = 20;
-  let ggg
-  let mario
+  let bgX = 0,
+    moveSpeed = 20;
+  var GRAVITY = 1;
+  var JUMP = 15;
+  let platform,ledge;
+  let mario;
+  let player;
   this.setup = function() {
-    ggg = loadImage("https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2Ftop_ground.png?v=1595568970498");
-    mario = new Mario()
+    platform = loadImage("https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2Ftop_ground.png?v=1595568970498");
+    ledge = loadImage("https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2Fledge.png?v=1595738720120")
+    player = new Mario();
   };
 
   this.draw = function() {
     background(200, 66, 100);
     image(this.sceneManager.fImage, bgX, 227);
     image(this.sceneManager.bImage, bgX, 265);
-    image(ggg, 0, 540);
-    //image(startGround, 0, 540);
-    text(this.sceneArgs+" Mode",210,300);
-    text(this.sceneManager.fImage.width,210,350);
-    mario.show()
+    image(ledge, 150, 540);
+    image(ledge, 400, 540);
+    //image(platform, 0, 540);
+    text(this.sceneArgs + " Mode", 210, 300);
+    text(this.sceneManager.fImage.width, 210, 350);
+    player.show();
   };
-    this.keyPressed = function(){
-    if(keyCode === RIGHT_ARROW){
-      if (mario.canMoveRight()){
-        mario.moveRight();
-    } else {
-      moveBackgroundLeft();
+  this.keyPressed = function() {
+    if (keyCode === RIGHT_ARROW) {
+      if (player.canMoveRight()) {
+        player.moveRight();
+      } else {
+        moveBackgroundLeft();
+      }
+    } else if (keyCode === LEFT_ARROW) {
     }
-    }else if(keyCode === LEFT_ARROW){
-      
+  };
+  function moveBackgroundLeft() {
+    let minBgLeft = width - 2000;
+    if (bgX - moveSpeed > minBgLeft) {
+      bgX -= moveSpeed;
     }
   }
-  function moveBackgroundLeft(){
-  let minBgLeft = width-2000;
-  if (bgX - moveSpeed > minBgLeft){
-    bgX -= moveSpeed;
+
+  function moveBackgroundRight() {
+    if (bgX + moveSpeed < 0) {
+      bgX += moveSpeed;
+    }
+  }
+  class Mario {
+    constructor() {
+      this.x = width / 2;
+      this.y = 500;
+      this.speed = 25;
+    }
+    canMoveRight() {
+      if (this.x < width - (50 + this.speed)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    canMoveLeft() {
+      if (this.x > 50 + this.speed) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    moveLeft() {
+      this.x -= this.speed;
+    }
+
+    moveRight() {
+      this.x += this.speed;
+    }
+    jump() {}
+    show() {
+      fill(255);
+      rect(this.x, this.y, 20, 20);
+    }
   }
 }
-
-function moveBackgroundRight(){
-  if (bgX + moveSpeed < 0){
-    bgX += moveSpeed;  
-  }
-}
-class Mario{
-  constructor(){
-    this.x = width/2;
-    this.y = 500;
-    this.speed = 25;
-  }
-  canMoveRight(){
-   if (this.x < width - (50 + this.speed)){
-     return true;
-   } else {
-     return false;
-   }
-  }
-   canMoveLeft(){
-   if (this.x > 50 + this.speed){
-     return true;
-   } else {
-     return false;
-   }
-  }
-   moveLeft(){
-    this.x -= this.speed;
-  }
-  
-  moveRight(){
-    this.x += this.speed;
-  }
-  jump(){
-    
-  }
-  show(){
-    fill(255);
-    rect(this.x, this.y, 20, 20);  
-  }
-}  
-}
-
-
