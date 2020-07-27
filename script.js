929b8c41c7a45ce77ea7aa910d1669bd9484e7c4
Glitch (hello-webpage) <none>
2020-07-27 01:09:31 +0000

    💐🕍 Checkpoint ./script.js:18078033/1505

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ function Intro() {
   };
   this.mousePressed = function() {
     // Switch to Game scene when user click any of the buttons
-    if (choice != "") {
+    if (choice != "") { // Make sure user's is on the buttons
       this.sceneManager.showScene(Game, choice);
     }
   };
@@ -105,25 +105,29 @@ function Intro() {
 }
 
 function Game() {
-  let bgX = 0,MariolastX=0;
-  let GRAVITY = 1,
-    JUMP = 15;
+  let MariolastX=0,GRAVITY = 1,JUMP = 15;
   let platform, ledges, mario, ledgeImg, longledgeImg, bgImg;
   this.setup = function() {
+    // Load Images
     bgImg = loadImage("https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2Fbg.png?v=1595800295790");
     ledgeImg = loadImage("https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2Fledge.png?v=1595738720120");
     longledgeImg = loadImage("https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2Flongledge.png?v=1595801236364");
+    
+    // Create Mario 
     mario = createSprite(width / 2, 515);
     mario.scale = 2.2;
     mario.addAnimation("normal","https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2FRunning-mario_01.png?v=1595741137506","https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2FRunning-mario_02.png?v=1595799759140","https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2FRunning-mario_03.png?v=1595799765213","https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2FStanding-mario.png?v=1595741033822");
     mario.addAnimation("move","https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2FStanding-mario.png?v=1595741033822","https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2FRunning-mario_01.png?v=1595741137506","https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2FJumping-mario.png?v=1595741095055","https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2FStanding-mario.png?v=1595741033822");
 
-    platform = createSprite(450, 570);
+    // Create Mario 
+    platform = createSprite(440, 570);
     platform.addAnimation("normal","https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2Fstartledge.png?v=1595801238081");
 
+    // Create Ledges Group
     ledges = new Group();
-    camera.position.y = height / 2;
-    mario.velocity.x = 4; // Mario ca
+
+    // Mario will move forward at the speed of 4
+    mario.velocity.x = 4; 
   };
 
   this.draw = function() {
@@ -132,7 +136,7 @@ function Game() {
     camera.off();
     this.showBackground();
     camera.on();
-    chickCollision();
+    checkCollision();
     marioMove();
     spawnLedges();
     text(MariolastX,mario.position.x,200)
@@ -148,7 +152,8 @@ function Game() {
     // Record last Mario,to check if Mario stuck at the ledge side
     MariolastX = mario.position.x
   }
-  function chickCollision() {
+  function checkCollision() {
+    // If Mario is on the platform or ledges, y value stays the same
     mario.velocity.y += GRAVITY;  
     if (mario.collide(platform) || mario.collide(ledges)) {
       if(mario.position.x>MariolastX){
@@ -157,25 +162,26 @@ function Game() {
       }
     }
   }
+  
+  /*Implementation of sound or body mode in function marioMove()
+if(this.sceneArgs==="sound"){
+
+}else{
+
+}
+*/
   function marioMove() {
-    // Use space key to move mario
+    // While receibe user input, Mario jumps
     if (keyWentDown(" ")) {
-      mario.changeAnimation("move");
+      mario.changeAnimation("move"); 
       mario.animation.rewind();
-
       mario.velocity.y = -JUMP;
-      if (mario.position.x < 220) {
-        mario.velocity.x = 30;
-      }
-
-      if (bgX - 50 > width - foregroundImg.width) {
-        bgX -= 50;
-      }
     }
   }
   function spawnLedges() {
     //spawn ledges
-    if (frameCount % 100 === 0 &&  mario.position.x>MariolastX) { // if Mario stuck at the ledge side, don't create new ledge
+    if (frameCount % 100 === 0 &&  mario.position.x>MariolastX) { 
+      // if Mario stuck at the ledge side, don't create new ledge
       let longledge = createSprite(mario.position.x + width, random(520,610));
       longledge.addImage(longledgeImg);
       ledges.add(longledge);
@@ -190,12 +196,8 @@ function Game() {
   }
 
   this.showBackground = function() {
-    image(bgImg, -mario.position.x%1600, 200);
-
-    //image(this.sceneManager.fImage, bgX, 227);
-    //image(this.sceneManager.bImage, bgX, 265);
+    image(bgImg, -mario.position.x%1500, 200);
   };
 }
 
-/*text(this.sceneArgs + " Mode", 210, 300);
-    text(mario.position.x, 210, 380);*/
+
