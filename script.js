0965f49c61e1634cacb25875ba7e2b04bda2d80e
Glitch (adaptable-melon-bow) <none>
2020-07-29 23:30:19 +0000

    🎳🖐 Checkpoint ./script.js:18078033/163

--- script.js
+++ script.js
@@ -7,17 +7,21 @@
 
 let foregroundImg, backgroundImg, groundImg, title;
 let introSong,coinSound,jumpSound
+let gameBackgroungImg;
 function preload() {
   // set the global sound format
   soundFormats('mp3')
-  // load background Images
+  // Materials for Intro
   foregroundImg = loadImage("https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2Fsky_2.png?v=1595568118809");
   backgroundImg = loadImage("https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2Fsky.png?v=1595568225904");
   groundImg = loadImage("https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2Ftop_ground.png?v=1595568970498");
   title = loadImage("https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2FSuper.png?v=1595626658046");
-  //
-  coinSound = loadSound("https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2FCoin.mp3?v=1596004574113")
-  jumpSound = loadSound("https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2FJump.mp3?v=1596005029268")
+  // Materials for Game
+  // jingle/music
+  coinSound = loadSound("https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2FCoin.mp3?v=1596004574113");
+  jumpSound = loadSound("https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2FJump.mp3?v=1596005029268");
+  
+  
 }
 function setup() {
   createCanvas(500, 600);
