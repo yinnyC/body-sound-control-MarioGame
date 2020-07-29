6c39d8e58ec2fb1bb4a8821faf6092ed02b35f8d
Glitch (hello-webpage) <none>
2020-07-29 00:09:02 +0000

    👰🌺 Checkpoint ./game.js:18078033/1:18143289/144

--- game.js
+++ game.js
@@ -4,7 +4,11 @@
  *    loop, noFill, noLoop, noStroke, random, rect, round, stroke, sqrt, text, width
  *    frameCount,UP_ARROW, DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW,Gameover
  */
-
+// 		textSize(32);
+// 		textAlign(CENTER);
+// 		text("Command Mario", width/2, height/2);
+// 		myRec.onResult = showResult;
+// 		myRec.start();
 function Game() {
   let MariolastX, GRAVITY, JUMP;
   let platform,
