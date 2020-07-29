baa8ef416c2579213f0f38801e9f1eae4484bdd8
Glitch (hello-webpage) <none>
2020-07-29 03:49:16 +0000

    🐬💏 Checkpoint ./instruction.js:18078033/1448 ./.glitch-assets:18078033/768

--- instruction.js
+++ instruction.js
@@ -6,35 +6,17 @@
  */
 
 function instruction() {
-  let bgX 
+  let bgX,demoNothing,demoJump; 
   this.enter = function(){
     bgX = 0
-    
+    demoNothing = loadImage("https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2FIMG_8147.JPG?v=1595994325897")
+    demoJump = loadImage("https://cdn.glitch.com/075b311a-0371-463a-a6ba-c4f6c09e32cb%2FIMG_8148.JPG?v=1595994325171")
   }
   this.draw = function() {
     background(210, 90, 100);
     this.showBackground();
     this.moveBackground();
-    rectMode(CENTER);
-    noStroke();
-    rect(width/2,height/2,400,470,8,8,8,8);
-
-    textFont("Rubik Mono One");
-    textSize(30);
-    textAlign(CENTER);
-    text("How To Play", width/2,120);
-    fill('red')
-    rect(width/2,175,120,30,20,20,20,20);
-    fill('blue')
-    rect(width/2,240,120,30,20,20,20,20);
-    textSize(12);
-    fill(255)
-    text("Sound Mode",width/2,180);
-    text("Body Mode", width/2,260);
-    textSize(8);
-    fill(0)
-    text("You can control Mario with voice,\njust say: \"Jump! \"",width/2,215);
-    
+    this.info();
   }
     this.mousePressed = function() {
       this.sceneManager.showScene(Intro);
@@ -49,5 +31,26 @@ function instruction() {
     bgX -= 2;
     bgX %=1024
   };
+  this.info=function(){
+    rectMode(CENTER);
+    noStroke();
+    rect(width/2,height/2,400,470,8,8,8,8);
+    textFont("Rubik Mono One");
+    textSize(30);
+    textAlign(CENTER);
+    text("How To Play", width/2,120);
+    fill('red')
+    rect(width/2,175,120,30,20,20,20,20);
+    fill('blue')
+    rect(width/2,260,120,30,20,20,20,20);
+    textSize(12);
+    fill(255)
+    text("Sound Mode",width/2,180);
+    text("Body Mode", width/2,265);
+    textSize(10);
+    fill(0)
+    text("You can control Mario with voice,\njust say: \"Jump! \"",width/2,215);
+    
+  }
   
 }
